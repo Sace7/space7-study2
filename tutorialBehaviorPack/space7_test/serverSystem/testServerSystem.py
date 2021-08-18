8d5a0b94f84c51aed1eef6b839b3a90b6c264458
丁鼎 <admin@mcd7.com>
2021-08-18 17:52:17 +0800

    丁鼎测试修改

--- tutorialBehaviorPack/space7_test/serverSystem/testServerSystem.py
+++ tutorialBehaviorPack/space7_test/serverSystem/testServerSystem.py
@@ -19,5 +19,6 @@ class TestServerSystem(serverApi.GetServerSystemCls()):
     # 监听到事件之后,进行逻辑处理的方法
     def 事件处理方法(self, args):
         print "111111111"
+        print "222222222"
         # 进行逻辑处理, 比如生成特效, 播放音效之类
         pass
